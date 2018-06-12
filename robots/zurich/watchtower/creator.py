40d66a3a3d6160a2fedfac1fa151a17097735351
Eric Lu <eric565648@gmail.com>
2018-06-12 16:50:38 +0200

    correct watchtower 01~09

--- robots/zurich/watchtower/creator.py
+++ robots/zurich/watchtower/creator.py
@@ -1,4 +1,9 @@
-for x in xrange(1,51):
+for x in xrange(1,10):
+    filename = 'watchtower0'+str(x)+'.robot.yaml'
+    file = open(filename, 'w+')
+    file.write("owner: eth_idsc \nhostname: watchtower0"+str(x) +"\nusername: mom\ndescription: watchtower, RaspberryPi with Camera\nlog:")
+    file.close()
+for x in xrange(10,51):
     filename = 'watchtower'+str(x)+'.robot.yaml'
     file = open(filename, 'w+')
     file.write("owner: eth_idsc \nhostname: watchtower"+str(x) +"\nusername: mom\ndescription: watchtower, RaspberryPi with Camera\nlog:")
